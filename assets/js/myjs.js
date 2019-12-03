ac0f7ba735b3524edca6568836a0054ddf1c65a6
Nata de koko <57607762+natadekoko97@users.noreply.github.com>
2019-12-03 23:08:43 +0700

    Add files via upload

---
+++ assets/js/myjs.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    console.log('test');
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".sticky-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+});
+
+
+
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
+var swiper = new Swiper('.swiper-container2', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    }
+  });
